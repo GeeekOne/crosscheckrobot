101620536e50e7a179016d185b31c884bc5a00d1
GeeekOne <@geeekone>
2025-02-17 17:19:57 +0200

    add delete service messages

--- handlers/group.py
+++ handlers/group.py
@@ -1,1 +1,14 @@
-# 
+from aiogram import Router, F, types
+
+group_router = Router()
+
+# Удаление сообщений о входе в чат
+@group_router.message(F.new_chat_members)
+async def delete_join_message(message: types.Message):
+    await message.delete()
+
+
+# Удаление сообщений о выходе из чата
+@group_router.message(F.left_chat_member)
+async def delete_leave_message(message: types.Message):
+    await message.delete()
