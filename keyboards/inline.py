e4ae3281cdf923abd652bc6e592f045eb8f95981
GeeekOne <@geeekone>
2025-02-17 15:49:09 +0200

    move there verify_kb

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,5 +1,10 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+# Кнопка для проверки
+verify_kb = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton(text="Я не бот", callback_data="not_a_bot")]]
+)
+
 admin_menu = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Посты", callback_data="posts_menu")],
 ])
