5c5053af4f975febb8bdd25d1bd79f0b8d9f9517
GeeekOne <@geeekone>
2025-03-26 19:12:09 +0200

    rename file

--- handlers/admin_private.py
+++ handlers/admin_private.py
@@ -5,16 +5,14 @@ from filters.chat_types import ChatTypeFilter, IsAdmin
 
 
 admin_router = Router()
-admin_router.message.filter(types.ChatTypeFilter(['private']), IsAdmin())
+admin_router.message.filter(ChatTypeFilter(['private']), IsAdmin())
 
 
 @admin_router.message(Command("res"))
-async def cmd_start(message: types.Message, bot: Bot):
-    botname = await bot.get_my_name()
+async def cmd_res(message: types.Message, bot: Bot):
     await message.answer("restart now")
 
 
 @admin_router.message(Command("root"))
-async def cmd_start(message: types.Message, bot: Bot):
-    botname = await bot.get_my_name()
+async def cmd_root(message: types.Message, bot: Bot):
     await message.answer("root enabled")
